c1b445bf18e7f01892c217cd2b7138d0f69bd957
Matthew Correia <matthewcorreia@Matthews-MacBook-Air.local>
2025-01-23 16:50:13 -0500

    Some changes

--- Age check/age_check_script.js
+++ Age check/age_check_script.js
@@ -1,7 +1,21 @@
 let age = Number(prompt("Enter your age" , "19"));
 checkAge(age)
 
+// Age check using ?
 function checkAge(age){
     return age > 18 ? True :
     confirm("Did your parents give you permission?");
+}
+
+//Age check using ||
+function checkAge(age){
+    return age > 18 || confirm("Did your parents give you permission?");
+}
+
+//Age check using if else
+function checkAge(age){
+    if(age > 18){
+        return True;
+    }
+    return confirm("Did your parents give you permission?");
 }
